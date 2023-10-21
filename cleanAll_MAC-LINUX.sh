b98c0a8854d00a7f8d3d7cb3628601a68dfdbac8
tim-bsm <125408219+tim-bsm@users.noreply.github.com>
2023-10-21 20:58:13 +0200

    example in chapter done

--- cleanAll_MAC-LINUX.sh
+++ cleanAll_MAC-LINUX.sh
@@ -1,6 +1,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/ads/watermark/cleanWatermark_MAC-LINUX.sh
+$SCRIPT_DIR/settings/watermark/cleanWatermark_MAC-LINUX.sh
 $SCRIPT_DIR/document/cleanDocument_MAC-LINUX.sh
-$SCRIPT_DIR/content/cleanContent_MAC-LINUX.sh
-$SCRIPT_DIR/settings/header/cleanHeader_MAC-LINUX.sh
+$SCRIPT_DIR/settings/config/cleanConfig_MAC-LINUX.sh
