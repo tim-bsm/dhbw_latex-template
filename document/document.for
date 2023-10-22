2aa43920a6a4b11f734f55685bb39fa9dacd8b07
tim-bsm <125408219+tim-bsm@users.noreply.github.com>
2023-10-22 22:19:21 +0200

    todo: readme form.tex

--- document/document.for
+++ document/document.for
@@ -7,4 +7,6 @@
 \addvspace {10\p@ }
 \contentsline {formel}{\numberline {1.1} Ohm'sches Gesetz}{1}{equation.1.1.1}%
 \addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
 \providecommand \tocbasic@end@toc@file {}\tocbasic@end@toc@file 
